e7155dc8d759920f022704ab64a6371f2beb993e
2-ha-0 <alohaquarium@gmail.com>
2025-09-06 17:41:22 +0900

    fix: prisma seed 오류 수정

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,33 @@
-
-
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.create({
-        data: {
-            email: 'a@a.com',
-            password: await bcrypt.hash('123456', 10),
-            name: 'Test User'
-        },
-    });
+  await prisma.user.create({
+    data: {
+      email: 'a@a.com',
+      password: await bcrypt.hash('123456', 10),
+      name: 'Test User',
+    },
+  });
 
-    await prisma.user.create({
-        data: {
-            email: 'b@b.com',
-            password: await bcrypt.hash('123456', 10),
-            name: 'Admin User',
-        },
-    });
+  await prisma.user.create({
+    data: {
+      email: 'b@b.com',
+      password: await bcrypt.hash('123456', 10),
+      name: 'Admin User',
+    },
+  });
 
-    console.log('Seed data inserted');
+  console.log('Seed data inserted');
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
